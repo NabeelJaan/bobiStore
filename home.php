dc8920791f5a1b568aeb8fd8b9a1c63b0e906b7f
Muhammad Nabeel <47273004+NabeelJaan@users.noreply.github.com>
2022-12-07 09:45:13 +0500

    Initial commit

---
+++ home.php
@@ -0,0 +1,28 @@
+<?php
+
+    // Template Name: Home Page
+
+?>
+
+
+<?php get_header(); ?>
+
+
+
+
+
+
+<section>
+    <div class="container mx-auto">
+        <h1 class="text-7xl font-bold">Bobi Cosmatic Store</h1>
+    </div>
+</section>
+
+
+
+
+
+
+
+
+<?php get_footer(); ?>
